510b223e219a772fa0981ebc901bcecef16628ea
AnnaYarmolenko <93723763+AnnaYarmolenko@users.noreply.github.com>
2025-09-28 16:14:16 +0300

    Added HW files

---
+++ src/server.js
@@ -0,0 +1,56 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.PORT ?? 3030;
+
+app.use(express.json());
+app.use(cors());
+
+app.use(pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/notes', (req, res) =>{
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:id', (req, res) => {
+  res.status(200).json({ message: `Retrieved note with ID: ${req.params.id}` });
+});
+
+// Маршрут для тестування middleware помилки
+app.get('/test-error', (req, res) => {
+  // Штучна помилка для прикладу
+  throw new Error('Simulated server error');
+});
+
+// Middleware 404 (після всіх маршрутів)
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+//  Middleware для обробки помилок (останнє)
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: 'Simulated server error',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
