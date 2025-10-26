cdac9afa43cf1b80f7c32f8d2363feab7c3bc765
AnnaYarmolenko <93723763+AnnaYarmolenko@users.noreply.github.com>
2025-10-26 07:42:08 +0200

    Added HW files

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import {updateUserAvatar} from '../controllers/userController.js';
+import { Router } from 'express';
+import { authenticate } from '../middleware/authenticate.js';
+import { upload } from '../middleware/multer.js';
+
+const router = Router();
+
+router.patch('/user/me/avatar', authenticate, upload.single('avatar'), updateUserAvatar);
+
+export default router;
+
